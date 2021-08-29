436715afe9fad21120dfc913929196aabbfd39c0
Alex Cruz <mazdarider23@gmail.com>
2021-08-29 16:32:20 +0200

    Add SELinux Switch

--- src/com/crystal/mine/Miscellaneous.java
+++ src/com/crystal/mine/Miscellaneous.java
@@ -41,19 +41,101 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//Imports for SELinux Switch
+import android.content.SharedPreferences;
+import android.os.SELinux;
+import android.util.log;
+import com.crystal.utils.SuShell;
+import com.crystal.utils.SuTask;
+
 @SearchIndexable
 public class Miscellaneous extends SettingsPreferenceFragment {
+
+    private static final String TAG = "Miscellaneous";
+
+    private static final String SELINUX_CATEGORY = "selinux";
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
+
+    private SwitchPreference mSelinuxMode;
+    private SwitchPreference mSelinuxPersistence;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.category_miscellaneous);
         PreferenceScreen prefSet = getPreferenceScreen();
+
+    // SELinux
+    Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
+    mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
+    mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
+    mSelinuxMode.setOnPreferenceChangeListener(this);
+
+    mSelinuxPersistence = (SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
+    mSelinuxPersistence.setOnPreferenceChangeListener(this);
+    mSelinuxPersistence.setChecked(getContext()
+        .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE)
+        .contains(PREF_SELINUX_MODE));   
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSelinuxMode) {
+        boolean enabled = (Boolean) newValue;
+        new SwitchSelinuxTask(getActivity()).execute(enabled);
+        setSelinuxEnabled(enabled, mSelinuxPersistence.isChecked());
+        return true;
+      } else if (preference == mSelinuxPersistence) {
+        setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) newValue);
+        return true;
+        }
+        return false;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CRYSTAL;
+    }
+
+    private void setSelinuxEnabled(boolean status, boolean persistent) {
+      SharedPreferences.Editor editor = getContext()
+          .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+      if (persistent) {
+        editor.putBoolean(PREF_SELINUX_MODE, status);
+      } else {
+        editor.remove(PREF_SELINUX_MODE);
+      }
+      editor.apply();
+      mSelinuxMode.setChecked(status);
+    }
+
+    private class SwitchSelinuxTask extends SuTask<Boolean> {
+      public SwitchSelinuxTask(Context context) {
+        super(context);
+    }
+    @Override
+    protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
+        if (params.length != 1) {
+          Log.e(TAG, "SwitchSelinuxTask: invalid params count");
+          return;
+        }
+        if (params[0]) {
+        SuShell.runWithSuCheck("setenforce 1");
+        } else {
+        SuShell.runWithSuCheck("setenforce 0");
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+        // Did not work, so restore actual value
+        setSelinuxEnabled(SELinux.isSELinuxEnforced(), mSelinuxPersistence.isChecked());
+        }
+      }
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
