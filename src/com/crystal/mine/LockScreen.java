f7d8f6b31b38a344c7cf8c2b5e72c2de34e062c4
Beanstown <nbenis106@gmail.com>
2021-11-10 20:26:46 +0100

    Fingerprint authentication vibration [2/2]

--- src/com/crystal/mine/LockScreen.java
+++ src/com/crystal/mine/LockScreen.java
@@ -18,7 +18,9 @@ package com.crystal.mine;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -46,11 +48,16 @@ import java.util.Arrays;
 import java.util.List;
 
 @SearchIndexable
-public class LockScreen extends SettingsPreferenceFragment {
+public class LockScreen extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String FP_KEYSTORE = "fp_unlock_keystore";
+    private static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+    private static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
 
     private SystemSettingSwitchPreference mFingerprintUnlock;
+    private FingerprintManager mFingerprintManager;
+    private SystemSettingSwitchPreference mFingerprintSuccessVib;
+    private SystemSettingSwitchPreference mFingerprintErrorVib;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -59,6 +66,28 @@ public class LockScreen extends SettingsPreferenceFragment {
         PreferenceScreen prefSet = getPreferenceScreen();
         final Resources res = getResources();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+        final PackageManager mPm = getActivity().getPackageManager();
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintSuccessVib = findPreference(FINGERPRINT_SUCCESS_VIB);
+        mFingerprintErrorVib = findPreference(FINGERPRINT_ERROR_VIB);
+        if (mPm.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) &&
+                 mFingerprintManager != null) {
+            if (!mFingerprintManager.isHardwareDetected()){
+                prefSet.removePreference(mFingerprintSuccessVib);
+                prefSet.removePreference(mFingerprintErrorVib);
+            } else {
+                mFingerprintSuccessVib.setChecked((Settings.System.getInt(getContentResolver(),
+                        Settings.System.FP_SUCCESS_VIBRATE, 1) == 1));
+                mFingerprintSuccessVib.setOnPreferenceChangeListener(this);
+                mFingerprintErrorVib.setChecked((Settings.System.getInt(getContentResolver(),
+                        Settings.System.FP_ERROR_VIBRATE, 1) == 1));
+                mFingerprintErrorVib.setOnPreferenceChangeListener(this);
+            }
+        } else {
+            prefSet.removePreference(mFingerprintSuccessVib);
+            prefSet.removePreference(mFingerprintErrorVib);
+        }
 
         mFingerprintUnlock = (SystemSettingSwitchPreference) findPreference(FP_KEYSTORE);
 
@@ -93,5 +122,21 @@ public class LockScreen extends SettingsPreferenceFragment {
                     final List<String> keys = super.getNonIndexableKeys(context);
                     return keys;
                 }
-            };
+    };
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mFingerprintSuccessVib) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FP_SUCCESS_VIBRATE, value ? 1 : 0);
+            return true;
+        } else if (preference == mFingerprintErrorVib) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FP_ERROR_VIBRATE, value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
 }
