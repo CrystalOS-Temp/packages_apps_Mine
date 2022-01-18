6025c49d0938657544d1674612b7d8467ee45c58
Pranav Vashi <neobuddy89@gmail.com>
2022-01-18 14:36:50 +0530

    Add signal and wifi icons [2/2]
    ;)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I4c593201a7186cc98b93e3db576b01be56eb5c8d

--- src/com/crystal/mine/fragments/extras/StatusbarIcons.java
+++ src/com/crystal/mine/fragments/extras/StatusbarIcons.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.crystal.mine.extras;
+package com.crystal.mine.fragments.extras;
 
 import android.content.ContentResolver;
 import android.content.Context;
