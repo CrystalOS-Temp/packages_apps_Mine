1459e0c3030b8a020c0c28f75e6d4679be583119
Joey Huab <joey@evolution-x.org>
2022-03-06 23:21:15 +0100

    Crystal: User toggle for streaming apps [2/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/crystal/mine/fragments/Miscellaneous.java
+++ src/com/crystal/mine/fragments/Miscellaneous.java
@@ -63,14 +63,18 @@ public class Miscellaneous extends SettingsPreferenceFragment implements OnPrefe
     private static final String SELINUX_CATEGORY = "selinux";
     private static final String PREF_SELINUX_MODE = "selinux_mode";
     private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
+    
     private static final String PREF_FLASH_ON_CALL = "flashlight_on_call";
     private static final String PREF_FLASH_ON_CALL_DND = "flashlight_on_call_ignore_dnd";
     private static final String PREF_FLASH_ON_CALL_RATE = "flashlight_on_call_rate";
+    
+    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+    private static final String KEY_STREAM_SPOOF = "use_stream_spoof";
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.photo";
+    private static final String SYS_STREAM_SPOOF = "persist.sys.stream";
 
-    private static final String KEY_SPOOF = "use_photos_spoof";
-    private static final String SYS_SPOOF = "persist.sys.photo";
-
-    private SwitchPreference mSpoof;
+    private SwitchPreference mPhotosSpoof;
+    private SwitchPreference mStreamSpoof;
 
     private SystemSettingListPreference mFlashOnCall;
     private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
@@ -85,10 +89,15 @@ public class Miscellaneous extends SettingsPreferenceFragment implements OnPrefe
         ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        final String useSpoof = SystemProperties.get(SYS_SPOOF, "1");
-        mSpoof = (SwitchPreference) findPreference(KEY_SPOOF);
-        mSpoof.setChecked("1".equals(useSpoof));
-        mSpoof.setOnPreferenceChangeListener(this);
+        final String usePhotosSpoof = SystemProperties.get(SYS_PHOTOS_SPOOF, "1");
+        mPhotosSpoof = (SwitchPreference) findPreference(KEY_PHOTOS_SPOOF);
+        mPhotosSpoof.setChecked("1".equals(usePhotosSpoof));
+        mPhotosSpoof.setOnPreferenceChangeListener(this);
+
+        final String useStreamSpoof = SystemProperties.get(SYS_STREAM_SPOOF, "1");
+        mStreamSpoof = (SwitchPreference) findPreference(KEY_STREAM_SPOOF);
+        mStreamSpoof.setChecked("1".equals(useStreamSpoof));
+        mStreamSpoof.setOnPreferenceChangeListener(this);
 
         // SELinux
         Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
@@ -129,9 +138,11 @@ public class Miscellaneous extends SettingsPreferenceFragment implements OnPrefe
             new SwitchSelinuxTask(getActivity()).execute(enabled);
             setSelinuxEnabled(enabled, mSelinuxPersistence.isChecked());
             return true;
+
         } else if (preference == mSelinuxPersistence) {
             setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) newValue);
             return true;
+
         } else if (preference == mFlashOnCall) {
             int value = Integer.parseInt((String) newValue);
             Settings.System.putInt(resolver,
@@ -139,19 +150,30 @@ public class Miscellaneous extends SettingsPreferenceFragment implements OnPrefe
             mFlashOnCallIgnoreDND.setVisible(value > 1);
             mFlashOnCallRate.setVisible(value != 0);
             return true;
+
         } else if (preference == mFlashOnCallRate) {
             int value = (Integer) newValue;
             Settings.System.putInt(resolver,
                     Settings.System.FLASHLIGHT_ON_CALL_RATE, value);
             return true;
-        }
-        else if (preference == mSpoof) {
+
+        } else if (preference == mPhotosSpoof) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.USE_PHOTOS_SPOOF, value ? 1 : 0);
-            SystemProperties.set(SYS_SPOOF, value ? "1" : "0");
+            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "1" : "0");
             Toast.makeText(getActivity(),
                     (R.string.photos_spoof_toast),
+                    Toast.LENGTH_LONG).show();
+            return true;
+            
+        } else if (preference == mStreamSpoof) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.USE_STREAM_SPOOF, value ? 1 : 0);
+            SystemProperties.set(SYS_STREAM_SPOOF, value ? "1" : "0");
+            Toast.makeText(getActivity(),
+                    (R.string.stream_spoof_toast),
                     Toast.LENGTH_LONG).show();
             return true;
         }
