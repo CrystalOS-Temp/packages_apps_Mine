dc9c43cefee2dd59b317e51f59fcfa49553b5805
romanbb <romanbirg@gmail.com>
2022-09-19 14:17:52 +0200

    Crystal: Add quick settings pull down with one finger [2/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/crystal/mine/fragments/QuickSettings.java
+++ src/com/crystal/mine/fragments/QuickSettings.java
@@ -16,6 +16,7 @@
 package com.crystal.mine.fragments;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -42,14 +43,41 @@ import java.util.Arrays;
 import java.util.List;
 
 @SearchIndexable
-public class QuickSettings extends SettingsPreferenceFragment {
+public class QuickSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener{
+
+    private ListPreference mQuickPulldown;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.category_quicksettings);
-        PreferenceScreen prefSet = getPreferenceScreen();
+        
+        final Context mContext = getActivity().getApplicationContext();
+        final ContentResolver resolver = mContext.getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
 
+        int qpmode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0, UserHandle.USER_CURRENT);
+        mQuickPulldown = (ListPreference) findPreference("status_bar_quick_qs_pulldown");
+        mQuickPulldown.setValue(String.valueOf(qpmode));
+        mQuickPulldown.setSummary(mQuickPulldown.getEntry());
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mQuickPulldown) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, value,
+                    UserHandle.USER_CURRENT);
+            int index = mQuickPulldown.findIndexOfValue((String) newValue);
+            mQuickPulldown.setSummary(
+                    mQuickPulldown.getEntries()[index]);
+            return true;
+        }
+        return false;
     }
 
     @Override
