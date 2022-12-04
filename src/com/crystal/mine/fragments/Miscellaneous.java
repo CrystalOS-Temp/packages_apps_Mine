7602f17dd4c0045db1e1187f48efc29219b57ec3
Stallix <joey@evolution-x.org>
2022-12-04 21:26:10 +0100

    Crystal: User toggle for streaming apps [2/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/crystal/mine/fragments/Miscellaneous.java
+++ src/com/crystal/mine/fragments/Miscellaneous.java
@@ -23,6 +23,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.widget.Toast;
 
 import androidx.preference.ListPreference;
 import androidx.preference.SwitchPreference;
@@ -52,10 +53,12 @@ public class Miscellaneous extends SettingsPreferenceFragment implements OnPrefe
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
     private static final String KEY_SYSTEM_BOOST = "system_boost";
+    private static final String KEY_STREAM_SPOOF = "use_stream_spoof";
 
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
     private static final String SYS_SYSTEM_BOOST = "persist.sys.system.boost";
+    private static final String SYS_STREAM_SPOOF = "persist.sys.stream";
     private static final String SYS_RENDER_BOOST_THREAD = "persist.sys.perf.topAppRenderThreadBoost.enable";
     private static final String SYS_COMPACTION = "persist.sys.appcompact.enable_app_compact";
     private static final String SYS_BSERVICE_LIMIT = "persist.sys.fw.bservice_limit";
@@ -66,6 +69,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements OnPrefe
     private SwitchPreference mGamesSpoof;
     private SwitchPreference mSystemBoost;
     private SwitchPreference mPhotosSpoof;
+    private SwitchPreference mStreamSpoof;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -90,6 +94,11 @@ public class Miscellaneous extends SettingsPreferenceFragment implements OnPrefe
         mSystemBoost = (SwitchPreference) findPreference(KEY_SYSTEM_BOOST);
         mSystemBoost.setChecked(SystemProperties.getBoolean(SYS_SYSTEM_BOOST, false));
         mSystemBoost.setOnPreferenceChangeListener(this);
+
+        final String useStreamSpoof = SystemProperties.get(SYS_STREAM_SPOOF, "1");
+        mStreamSpoof = (SwitchPreference) findPreference(KEY_STREAM_SPOOF);
+        mStreamSpoof.setChecked("1".equals(useStreamSpoof));
+        mStreamSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -116,6 +125,15 @@ public class Miscellaneous extends SettingsPreferenceFragment implements OnPrefe
             SystemProperties.set(SYS_BSERVICE_AGE, value ? "8000" : "300000");
             SystemProperties.set(SYS_BSERVICE, value ? "true" : "false");
             SystemPropPoker.getInstance().poke();
+            return true;
+        } else if (preference == mStreamSpoof) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.USE_STREAM_SPOOF, value ? 1 : 0);
+            SystemProperties.set(SYS_STREAM_SPOOF, value ? "1" : "0");
+            Toast.makeText(getActivity(),
+                    (R.string.stream_spoof_toast),
+                    Toast.LENGTH_LONG).show();
             return true;
         }
         return false;
